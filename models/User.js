b8d6ee32ebcdb4c9f08e94f11a572706ad66f4e3
jpmalo <malomedia7@gmail.com>
2018-09-26 11:02:44 -0700

    .

---
+++ models/User.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        hash: {
+            type: DataTypes.STRING(1500),
+            allowNull: false
+        },
+        salt: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }); 
+    User.associate = function(models) {
+      models.User.hasMany(models.Scripts, {
+          onDelete: "cascade"
+      });
+    };
+
+    return User; 
+  };
---
+++ server.js
@@ -0,0 +1,48 @@
+// requirements
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const jwt = require("express-jwt");
+const authRoutes = require("./routes/auth-routes.js");
+const scriptRoutes = require("./routes/script-routes.js");
+const fileUpload = require('express-fileupload');
+
+//middleware
+const bodyParser = require('body-parser');
+
+
+//express setup
+const app = express();
+const PORT = process.env.PORT || 3001;
+const isDev = process.env.NODE_ENV === 'production';
+
+//fileupload middleware
+app.use(fileUpload())
+// Requiring our models for syncing
+const db = require(path.join(__dirname + '/models'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//routes
+app.use(express.static(path.join(__dirname + '/public')));
+app.use("/api/user", authRoutes);
+app.use(express.static(path.join(__dirname + '/scripts')));
+
+
+app.use(["/api/scripts"], jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+}));
+app.use("/api/scripts", scriptRoutes);
+
+
+
+db.sequelize.sync({ force: false, logging: console.log }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    })
+});
+
+
