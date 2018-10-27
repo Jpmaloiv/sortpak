cf64ecd909fa201aaf722a47cee781485e181b57
jpmalo <malomedia7@gmail.com>
2018-10-26 22:56:14 -0700

    .

--- routes/patient-routes.js
+++ routes/patient-routes.js
@@ -67,15 +67,15 @@ router.get("/search", (req, res) => {
         attributes: {
             exclude: ["updatedAt", "UserId"]
         },
-        include: [{
-            model: db.Scripts,
-            attributes: ["id", "patient", "processedOn", "status"],
-        },
-        {
-            model: db.Physicians,
-            attributes: ['id', 'firstName', 'lastName']
-        }
-        ]
+        // include: [{
+        //     model: db.Scripts,
+        //     attributes: ["id", "patient", "processedOn", "status"],
+        // },
+        // {
+        //     model: db.Physicians,
+        //     attributes: ['id', 'firstName', 'lastName']
+        // }
+        // ]
         
     }
     if (req.query.patientId) {
