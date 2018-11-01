750c331905ae70d94bf3d9eefe1fc4ecb1946b4f
jpmalo <malomedia7@gmail.com>
2018-10-31 20:51:47 -0700

    .

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -3,49 +3,34 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const authCtrl = require("../controller/auth/auth-ctrl.js");
+const authCtrl = require("../controller/auth/auth-ctrl");
 const fs = require('fs');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-var url = require('url');
+
 
 router.get("/search", (req, res) => {
-    // var url_parts = url.parse(req.url, true);
-    // var query = url_parts.query;
-    // var dbQuery = {
-    //         attributes: ['DrugName', 'DrugNdc'],
-    //         where: {
-    //             [Op.or]:  [{
-    //                 DrugName: {
-    //                     like: '%'+ query.s + '%'
-    //                 }
-    //             }, {
-    //                 DrugNdc: {
-    //                     like: '%' + query.s + '%'
-    //                 }
-    //             }]
-    //         }
-    //     }
     let searchParams = {
         where: {},
         attributes: {
-            exclude: ["createdAt","updatedAt", "UserId"]
-        }
+            exclude: ["createdAt", "updatedAt"]
+        },
     }
+  
+    console.log(searchParams);
     db.Products
         .findAll(searchParams)
         .then((response) => {
             res.json({
-               successs: true,
-               response: response
+                success: true,
+                response: response
             });
-            console.log(response)
         })
         .catch((err) => {
             console.error(err);
             res.status(500).json({ message: "Error (500): Internal Server Error", error: err })
         })
-})
+    })
 
 
 module.exports = router;
